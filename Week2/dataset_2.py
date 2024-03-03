fc30efd26c2dd20a93fd1814088ef82ad6b72d45
Carles Pregonas <carles.prego@gmail.com>
2024-03-03 20:09:33 +0100

    update

--- Week2/dataset_2.py
+++ Week2/dataset_2.py
@@ -4,33 +4,8 @@ from pathlib import Path
 import pandas as pd
 from pycocotools.mask import toBbox
 from detectron2.structures import BoxMode
+import glob
 
-
-def load_txt(path):
-    objects_per_frame = {}
-    with open(path, "r") as f:
-        for line in f:
-            line = line.strip()
-            fields = line.split(" ")
-
-            frame = int(fields[0])
-            track_id = int(fields[1])
-            class_id = int(fields[2])  
-            height = int(fields[3]) 
-            width = int(fields[4]) 
-         
-            if not(class_id == 1 or class_id == 2):
-              continue
-
-            mask = {'size': [int(fields[3]), int(fields[4])], 'counts': fields[5].encode('utf8')}
-
-            if frame not in objects_per_frame:
-                objects_per_frame[frame] = []
-            objects_per_frame[frame].append(
-              (frame, track_id, class_id, height, width, mask)
-            )
-
-    return objects_per_frame
 
 
 def load_seqmap(seqmap_filename):
@@ -45,73 +20,96 @@ def load_seqmap(seqmap_filename):
       max_frames[seq] = int(fields[3])
   return seqmap, max_frames
 
-def class_mapping_kitti_to_coco(path, part):
+def class_mapping_kitti_to_coco(img_dir, mode: str):
 
     mapped_classes = {1: 2, 2: 0}  # Mapping KITTY to COCO classes
-    if part == 'training':
-        mode = 'train'
-    else:
-        mode = part
+
+    if mode == "training":
+       mode = "train"
 
     seqmap_file = f'/ghome/group01/MCV-C5-G1/Week2/seqmaps/{mode}.seqmap'
-    sequences, _ = load_seqmap(seqmap_file)
+    imgs_anns, _ = load_seqmap(seqmap_file)
 
-    sequence_dir = os.path.join(path, "training", "image_02")
+    print(imgs_anns)
 
-    annotations = []
-    for seq in Path(sequence_dir).glob("*"):
-        sequence = seq.parts[-1]
+    dataset_dicts = []
+    id = 0
+    for idx, v in enumerate(imgs_anns):
+        record = {}
+        instance_dir = os.path.join(img_dir, "instances_txt", v + ".txt")
+        imgs_dir = os.path.join(img_dir, "training", "image_02", v)
 
-        if sequence not in sequences:
-            continue
+        objects_per_frame = {}
+        frames = []
+        with open(instance_dir, "r") as f:
+            for line in f:
+                line = line.strip()
+                fields = line.split(" ")
 
-        gt = load_txt(os.path.join(path, "instances_txt", sequence + ".txt"))
+                frame = int(fields[0])
+                track_id = int(fields[1])
+                class_id = int(fields[2])
 
-        for img_path in seq.glob("*.png"):
-          img_name = img_path.parts[-1]
-          frame = int(img_path.stem)  # Use stem to get the file name without extension
-          print(gt[frame][0][0])
-          break
-          try:
-            frame_gt = gt[frame]  # Retrieve ground truth for the current frame
-          except:
-            continue
-          if not frame_gt:
-              continue
-          print(frame_gt)
-          ann = []
-          for _, _, class_id, height, width, mask in frame_gt:
+                if not (class_id == 1 or class_id == 2):
+                    continue  
 
-              if class_id != 1 and class_id != 2:
-                  continue
+                height = int(fields[3]) 
+                width = int(fields[4])
 
-              bbox = toBbox(mask).tolist()
-              ann.append(
-                  {
-                      "bbox": bbox,
-                      "bbox_mode": BoxMode.XYWH_ABS,
-                      "category_id": mapped_classes[class_id],
-                      "segmentation": mask,
-                      "keypoints": [],
-                      "iscrowd": 0,
-                  }
-              )
-          annotations.append(
-              {
-                  "file_name": str(img_path),
-                  "height": height,
-                  "width": width,
-                  "image_id": int(f"{sequence}{frame:05}"),
-                  "sem_seg_file_name": str(
-                      os.path.join(path, "instances", sequence, img_name)
-                  ),
-                  "annotations": ann,
-              }
-          )
-    return annotations
+                # New image record
+                if frame not in frames or len(frames)==0 and True:
+                    if len(frames) != 0 and len(dataset_dicts) != 0:
+                       dataset_dicts.append(record)
 
+                    frames.append(frame)
+
+                    for img in os.listdir(imgs_dir):
+                        
+                        image_frame = int(img.split('.')[0])
+                        
+                        if image_frame != frame:
+                           continue
+                        
+                        record = {}
+                        record["file_name"] = os.path.join(imgs_dir, img)
+                        record["image_id"] = id
+                        id += 1
+                        record["height"] = height
+                        record["width"] = width
+
+                        mask = {'size': [int(fields[3]), int(fields[4])], 'counts': fields[5].encode('utf8')}
+                        bbox = toBbox(mask).tolist()
+
+                        annotations = []
+                        annotations.append({
+                            "bbox": bbox,
+                            "bbox_mode": BoxMode.XYWH_ABS,
+                            "category_id": mapped_classes[class_id],
+                            "segmentation": mask,
+                            "keypoints": [],
+                            "iscrowd": 0
+                        })
+
+                        record["annotations"] = annotations
+
+                        break
+
+                # New object from the same image
+                elif frame in frames:
+
+                    record["annotations"].append({
+                            "bbox": bbox,
+                            "bbox_mode": BoxMode.XYWH_ABS,
+                            "category_id": mapped_classes[class_id],
+                            "segmentation": mask,
+                            "keypoints": [],
+                            "iscrowd": 0
+                        })
+
+    return dataset_dicts           
+                
 
 BASE_DIR = "/ghome/group01/mcv/datasets/C5/KITTI-MOTS"
 ann = class_mapping_kitti_to_coco(BASE_DIR, "training")
-#print(ann[0])
+print(ann[0])
 
